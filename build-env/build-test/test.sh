3f25b902f08c208094391807cdc51466128283d7
mikemeding <mikeymeding@gmail.com>
2016-02-12 13:01:24 -0500

    added as many machine profiles as i could find

--- build-env/build-test/test.sh
+++ build-env/build-test/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # CuraEngine slice [-v] [-p] [-j <settings.json>] [-s <settingkey>=<value>] [-g] [-e] [-o <output.gcode>] [-l <model.stl>]
 
-CuraEngine slice -v -j prusa_i3.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
+#CuraEngine slice -v -j prusa_i3.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
 #CuraEngine slice -v -j ultimaker2.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
+CuraEngine slice -v -j maker_starter.json -g -e -o "output/test.gcode" -l ../../models/test-models/balanced_die_version_2.stl 
 
