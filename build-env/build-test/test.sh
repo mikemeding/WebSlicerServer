700115e57f652e67cd75f45c77ed4ef25e386b2e
mikemeding <mikeymeding@gmail.com>
2016-02-11 20:52:01 -0500

    did some command line testing. found the bug

--- build-env/build-test/test.sh
+++ build-env/build-test/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # CuraEngine slice [-v] [-p] [-j <settings.json>] [-s <settingkey>=<value>] [-g] [-e] [-o <output.gcode>] [-l <model.stl>]
 
-../../CuraEngine-master/build/CuraEngine slice -v -j ultimaker2.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
+CuraEngine slice -v -j prusa_i3.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
+#CuraEngine slice -v -j ultimaker2.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
 
