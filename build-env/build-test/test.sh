646a96bca004584a636584bd0cc1786e80215b38
mikemeding <mikeymeding@gmail.com>
2016-02-25 17:57:39 -0500

    fixed density issue. FUCKING FINALLY.

--- build-env/build-test/test.sh
+++ build-env/build-test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # CuraEngine slice [-v] [-p] [-j <settings.json>] [-s <settingkey>=<value>] [-g] [-e] [-o <output.gcode>] [-l <model.stl>]
 
-#CuraEngine slice -v -j prusa_i3.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
+CuraEngine slice -v -j prusa_i3.json -g -e -o "output/test.gcode" -l ../../models/test-models/balanced_die_version_2.stl
 #CuraEngine slice -v -j ultimaker2.json -g -e -o "output/test.gcode" -l ../../models/ControlPanel.stl 
-CuraEngine slice -v -j maker_starter.json -g -e -o "output/test.gcode" -l ../../models/test-models/balanced_die_version_2.stl 
-
+#CuraEngine slice -v -j maker_starter.json -g -e -o "output/test.gcode" -l ../../models/test-models/balanced_die_version_2.stl 
+#CuraEngine slice -v -j machines/bq_hephestos_2.json -g -e -o "output/test.gcode" -l ../../models/test-models/balanced_die_version_2.stl
